c821783070e156a3d458aa5879f39b7ad7539a51
reaby <petri.jarvisalo@gmail.com>
2016-12-21 10:23:22 +0200

    adding sample content

--- server.js
+++ server.js
@@ -39,21 +39,19 @@ var auth = function (req, res, next) {
     function unauthorized(res) {
         res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
         return res.sendStatus(401);
-    };
+    }
 
     var user = basicAuth(req);
 
     if (!user || !user.name || !user.pass) {
         return unauthorized(res);
     }
-    ;
 
     if (user.name === config.adminUsername && user.pass === config.adminPass) {
         return next();
     } else {
         return unauthorized(res);
     }
-    ;
 };
 
 app.get('/admin', auth, function (req, res) {
